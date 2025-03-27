8ab398189773500d336f2f202ce0df56bc97307d
rsinema <rileysinema@gmail.com>
2025-03-27 10:19:18 -0600

    fixed bucket name

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "mc_server_backup_bucket" {
-    bucket = "rsinema-${var.server_name}-backup"
+bucket = lower("rsinema-${var.server_name}-backup")
 
-    tags = {
-      Name = "${var.server_name}-backup"
-    }
+  tags = {
+    Name = "${var.server_name}-backup"
+  }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
